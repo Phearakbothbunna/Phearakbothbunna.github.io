639f26b1c421f932c64c27922dfb0e465fe2e7c3
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2022-07-04 02:42:16 -0700

    new changes

---
+++ app.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.section');
+// All buttons
+const sectBtns = document.querySelectorAll('.controls');
+// Each button
+const sectBtn = document.querySelectorAll('.control');
+// Body
+const allSects = document.querySelectorAll('.main-content');
+
+
+//function PageTransitions(){
+    // Button click active class
+    // Only show the active ones 
+    //for (let i = 0; i < sectBtn.length; i++){
+        //sectBtn
+    //}
+//}
